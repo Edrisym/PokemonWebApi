47989a12b2a74bf9db3096686844ccc4e2dd4815
EdrisGhafori <edrismoavenghafouri@gmail.com>
2023-09-02 16:03:30 +0330

    Adding repositories and interfaces for pokemon

--- PokemonWebApi/Interfaces/IPokemonRepository.cs
+++ PokemonWebApi/Interfaces/IPokemonRepository.cs
@@ -6,6 +6,10 @@ namespace PokemonWebApi.Interfaces
     public interface IPokemonRepository
     {
         ICollection<Pokemon> GetPokemons();
+        Pokemon GetPokemon(int id);
+        Pokemon GetPokemon(stringn name);
+        decimal GetRating(Pokemon pokeId);
+        bool PokemonExist(Pokemon pokeId);
     }
 }
 
