01dc6e7c1837ba9d694b10fb78f632933ae5477f
Edrisym <edrismoavenghafouri@gmail.com>
2023-09-08 22:03:26 +0330

    Adding Owner Interface and class raw repository

---
+++ PokemonWebApi/Repositories/OwnerRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using PokemonWebApi.Interfaces;
+using PokemonWebApi.Models;
+
+namespace PokemonWebApi.Repositories
+{
+    public class OwnerRepository : IOwnerRepository
+    {
+        public OwnerRepository()
+        {
+        }
+
+        public Owner GetOwner(int ownerid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Owner> GetOwnerOfAPokemon(int pokeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Owner> GetOwners()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Pokemon> GetPokemonByOwner(int ownerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool OwnerExists(int ownerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
